95e7a9888234e9eaa407973a4d0ce169b5033ea2
Jenisha-bounteous <jenisha.j@bounteous.com>
2026-07-14 13:24:41 +0530

    fix: update arrow-right icon styling

--- src/components/SIcons/Icons/arrow-right.tsx
+++ src/components/SIcons/Icons/arrow-right.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ArrowRight = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg viewBox="-2 -2 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M14.375 6.875L17.5 10L14.375 13.125M17.5 10H2.5"
       stroke="black"
